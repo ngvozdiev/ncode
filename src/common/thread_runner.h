4f614e1186985bb670e7b7b77c026ccb3232701c
Nik <nik.gvozdiev@gmail.com>
2017-03-10 20:43:18 +0000

    Changes to thread runner API

--- src/common/thread_runner.h
+++ src/common/thread_runner.h
@@ -6,6 +6,7 @@
 #include <thread>
 #include <vector>
 #include <mutex>
+#include "ptr_queue.h"
 
 namespace ncode {
 
@@ -41,50 +42,122 @@ void RunInParallel(const std::vector<T>& arguments,
   }
 }
 
-// Runs and maintains a number of instances of ProcessorBase.
-template <typename Input, typename Result, typename Processor,
-          size_t QueueSize = 1000>
-class ThreadPool {
+// Runs and maintains a number of threads that process incoming data. Each
+// thread can be associated with an instance of Data.
+template <typename T>
+class ThreadBatchProcessor {
  public:
-  using Factory = std::function<Processor()>;
-  ThreadPool(size_t processor_count, Factory factory) {
-    for (size_t i = 0; i < processor_count; ++i) {
-      processors_.emplace_back(factory());
+  ThreadBatchProcessor(size_t threads)
+      : thread_count_(threads),
+        to_kill_(false),
+        batch_arguments_(nullptr),
+        batch_f_(nullptr),
+        number_active_(0) {
+    active_threads_.resize(thread_count_, false);
+    for (size_t i = 0; i < threads; ++i) {
+      threads_.emplace_back([this, i] { DoWork(i); });
+    }
+  }
+
+  ~ThreadBatchProcessor() {
+    {
+      std::lock_guard<std::mutex> lock(mu_);
+      to_kill_ = true;
+      new_batch_ready_.notify_all();
+    }
+
+    for (size_t i = 0; i < thread_count_; ++i) {
+      threads_[i].join();
+    }
+  }
+
+  void RunInParallel(const std::vector<T>& arguments,
+                     std::function<void(const T&, size_t, size_t)> f) {
+    {
+      std::unique_lock<std::mutex> lock(mu_);
+      batch_arguments_ = &arguments;
+      batch_f_ = &f;
+
+      batch_done_.resize(arguments.size(), false);
+
+      // Activate all threads.
+      std::fill(active_threads_.begin(), active_threads_.end(), true);
+      number_active_ = thread_count_;
+    }
+    new_batch_ready_.notify_all();
+    {
+      std::unique_lock<std::mutex> lock(mu_);
+      thread_done_.wait(lock, [this] { return number_active_ == 0; });
+
+      batch_arguments_ = nullptr;
+      batch_f_ = nullptr;
+      batch_done_.clear();
+
+      // Deactivate all threads.
+      std::fill(active_threads_.begin(), active_threads_.end(), false);
     }
   }
 
  private:
-  using IdAndInput = std::pair<uint64_t, std::unique_ptr<Input>>;
+  void DoWork(size_t thread_index) {
+    while (!to_kill_) {
+      std::unique_lock<std::mutex> lock(mu_);
+      new_batch_ready_.wait(lock, [this, thread_index] {
+        return to_kill_ || active_threads_[thread_index];
+      });
+      if (to_kill_) {
+        break;
+      }
 
-  struct ProcessorState {
-    ProcessorState(Processor processor, PtrQueue<Input, QueueSize>* input_queue)
-        : processor(processor) {
-      processor_thread = [this, input_queue] {
-        while (true) {
-          std::unique_ptr<Input> input = input_queue->ConsumeOrBlock();
-          if (!input) {
-            break;
-          }
+      const std::vector<T>& arguments = *batch_arguments_;
+      const std::function<void(const T&, size_t, size_t)>& f = *batch_f_;
 
-          processor.Process(input);
+      for (size_t i = 0; i < arguments.size(); ++i) {
+        if (!batch_done_[i]) {
+          batch_done_[i] = true;
+          lock.unlock();
+          f(arguments[i], i, thread_index);
+          lock.lock();
         }
-      };
+      }
+
+      active_threads_[thread_index] = false;
+      --number_active_;
+      if (number_active_ == 0) {
+        lock.unlock();
+        thread_done_.notify_one();
+      }
     }
+  }
 
-    Processor processor;
-    std::thread<void()> processor_thread;
-  };
+  // Number of threads.
+  size_t thread_count_;
 
-  // Work is added to this queue and processed by the processors.
-  PtrQueue<IdAndInput, QueueSize> input_queue_;
+  bool to_kill_;
+
+  // Done status of the current batch.
+  std::vector<bool> batch_done_;
+
+  // The arguments for the current batch.Either null if no batch, or points to
+  // the stack of RunInParallel.
+  const std::vector<T>* batch_arguments_;
+
+  // Function for the current batch.
+  std::function<void(const T&, size_t, size_t)>* batch_f_;
 
   // The processors.
-  std::vector<Processor> processors_;
+  std::vector<std::thread> threads_;
 
-  // Protects input_queue_.
+  std::condition_variable new_batch_ready_;
+  std::condition_variable thread_done_;
+
+  // Number of threads active and a vector indicating which threads are active.
+  size_t number_active_;
+  std::vector<bool> active_threads_;
+
+  // Mutex for all the condition variables.
   std::mutex mu_;
 };
-
 }  // namespace common
 
 #endif
--- src/common/thread_runner_test.cc
+++ src/common/thread_runner_test.cc
@@ -36,5 +36,31 @@ TEST_P(ThreadRunnerTestWithBatchSize, SimpleIO) {
 INSTANTIATE_TEST_CASE_P(SimpleThreadRunner, ThreadRunnerTestWithBatchSize,
                         ::testing::Values(1, 5, 20, 50), );
 
+class ThreadBatchProcessorTestWithBatchSize
+    : public ::testing::TestWithParam<int> {};
+
+TEST_P(ThreadBatchProcessorTestWithBatchSize, SimpleIO) {
+  ThreadBatchProcessor<int> batch_processor(GetParam());
+  std::mutex mu;
+
+  std::vector<int> args(20);
+  std::iota(args.begin(), args.end(), 0);
+
+  std::set<int> out;
+  batch_processor.RunInParallel(args, [&out, &mu](int i, size_t k, size_t t) {
+    Unused(k);
+    Unused(t);
+    std::lock_guard<std::mutex> lock(mu);
+    out.insert(i);
+  });
+
+  std::set<int> model(args.begin(), args.end());
+  ASSERT_EQ(model, out);
+}
+
+INSTANTIATE_TEST_CASE_P(SimpleThreadRunner,
+                        ThreadBatchProcessorTestWithBatchSize,
+                        ::testing::Values(1, 5, 20, 50), );
+
 }  // namespace
 }  // namespace ncode
